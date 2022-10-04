1f4e2edc9713b9f864f7f27e579405e9ae43a634
Guilherme Prates <contact@guilhermeprates.com>
2022-10-04 16:26:41 -0300

    Add player and first level files

---
+++ Source/Entrance/Entrance.gd
@@ -0,0 +1,7 @@
+class_name Entrance extends Node2D
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
---
+++ Source/Player/Player.gd
@@ -0,0 +1,7 @@
+class_name Player extends KinematicBody2D
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
