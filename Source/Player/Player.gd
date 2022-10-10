104151101718c4c314cf44a6d4b53d989df4c275
Guilherme Prates <contact@guilhermeprates.com>
2022-10-10 00:52:00 -0300

    AnimationPlayer and AnimationTree Test

--- Source/Player/Player.gd
+++ Source/Player/Player.gd
@@ -6,71 +6,54 @@ const SPEED: int = 200
 const JUMP_SPEED: int = 800
 const GRAVITY: int = 2500
 
-onready var sprite: AnimatedSprite = $AnimatedSprite
-onready var camera: Camera2D = $PlayerCamera
-
 export(int) var health_points: int = 200
 export(int) var mana_points: int = 100
 export(int) var base_attack: int = 10
 
-var velocity: Vector2 = Vector2.ZERO
-var is_jumping: bool = false
-var is_grounded: bool = true
+var _velocity: Vector2 = Vector2.ZERO
+var _jumping: bool = false
+var _attacking: bool = false
+var _grounded: bool = true
+
+onready var camera: Camera2D = $PlayerCamera
+onready var sprite: Sprite = $Sprite
+onready var animationTree: AnimationTree = $AnimationTree
+onready var animationTransition = animationTree.get("parameters/transition")
 
 func _ready() -> void:
 	_set_connections()
 
 func _physics_process(delta: float) -> void:
 	_get_input()
-	velocity.y += GRAVITY * delta
-	velocity = move_and_slide(velocity, Vector2.UP)
+	_velocity.y += GRAVITY * delta
+	_velocity = move_and_slide(_velocity, Vector2.UP)
 	
-	var was_grounded  = is_grounded
-	is_grounded = is_on_floor()
-	if was_grounded == null || is_grounded != was_grounded:
-		emit_signal("on_grounded_updated", is_grounded)
+	var was_grounded = _grounded
+	_grounded = is_on_floor()
+	if was_grounded == null || _grounded != was_grounded:
+		emit_signal("on_grounded_updated", _grounded)
+
+func _reset_state() -> void:
+	_velocity = Vector2.ZERO
 
 func _get_input():
-	velocity.x = Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
-	velocity.x = velocity.x * SPEED
+	_velocity.x = Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
+	_velocity.x = _velocity.x * SPEED
 	
-	if Input.is_action_pressed("jump") and !is_grounded:
-		sprite.play("jump")
-		if Input.is_action_pressed("attack"):
-			pass
-	elif Input.is_action_pressed("ducking") and is_grounded:
-		if Input.is_action_pressed("attack"):
-			pass
-		if velocity.x < 0:
-			sprite.flip_h = true
-			sprite.play("ducking")
-		elif velocity.x > 0:
-			sprite.flip_h = false
-			sprite.play("ducking")
-		else:
-			sprite.play("idle_ducking")
-			sprite.stop()
+
+	if _velocity.x > 0 and not _attacking:
+		sprite.flip_h = false
+		animationTree.set("parameters/movement/current", 1)
+	elif _velocity.x < 0 and not _attacking:
+		sprite.flip_h = true
+		animationTree.set("parameters/movement/current", 4)
 	else:
-		if Input.is_action_pressed("jump") and is_grounded:
-			is_jumping = true
-			velocity.y = -JUMP_SPEED
-		else:
-			is_jumping = false
-		
-		# TODO: manter o sprite de pulo até tocar no chão, mudar só se atacar
-		
-		if Input.is_action_pressed("attack"):
-			pass
-		
-		if velocity.x < 0:
-			sprite.flip_h = true
-			sprite.play("walk")
-		elif velocity.x > 0:
-			sprite.flip_h = false
-			sprite.play("walk")
-		else:
-			sprite.play("idle")
-			sprite.stop()
+		if not _attacking:
+			animationTree.set("parameters/movement/current", 1)
+#			
+#	if Input.get_action_strength("attack"):
+#		sprite.play("attacking")
+#		_is_attacking = true
 
 func _set_connections() -> void:
 	pass
