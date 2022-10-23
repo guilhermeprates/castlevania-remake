734424d54e5a05bd88322377f92ea7ea48400e58
igcarol <111294070+igcarol@users.noreply.github.com>
2022-10-23 00:13:40 -0300

    Add files via upload

---
+++ Enemies/Zombie.gd
@@ -0,0 +1,35 @@
+class_name Enemies extends KinematicBody2D
+
+const SPEED: int = 150
+const GRAVITY: int = 2500
+
+export(int) var health_points: int = 1
+
+var _velocity: Vector2 = Vector2.ZERO
+var _move_direction = -1
+var hitted = false #quando inimigo for acertado
+
+func _physics_process(delta: float) -> void:
+	_velocity.x = SPEED * _move_direction
+	_velocity.y += GRAVITY * delta
+	_velocity = move_and_slide(_velocity, Vector2.UP)
+	_set_animation()
+
+func _set_animation():
+	var anim = "run"
+	if hitted == true:
+		anim = "death"
+#	if $AnimationZombie.assigned_animation != anim:
+#		$anim.play(anim)
+
+func _on_Hitbox_body_entered(body: Node) -> void:
+	print("Player")
+	hitted = true #colidiu
+	health_points -= 1
+	yield(get_tree().create_timer(0.1), "timeout") #quando der o timeout, outra linha vai começar:
+	hitted = false
+	if health_points <1:
+		$AnimationZombie.play("death")
+		yield(get_tree().create_timer(0.3), "timeout")
+		queue_free()
+		get_node("Hitbox/CollisionHitbox").set_deferred("disable", true) #inimigo vai morrer sem ter problemas na colisão
