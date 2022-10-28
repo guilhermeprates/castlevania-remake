ccd2776439ec5b2fa9c1d0a268fe753bc486f819
igcarol <111294070+igcarol@users.noreply.github.com>
2022-10-28 10:35:51 -0300

    Add files via upload

--- Source/HUD/HUD.gd
+++ Source/HUD/HUD.gd
@@ -8,8 +8,8 @@ var timer = 300
 func _process(delta: float) -> void:
 	var hearts = PlayerVariables.hearts
 	var health_points = PlayerVariables.health_points
-	heartsLabel.text = "- " + str(hearts)
-	healthLabel.text = "P - " + str(health_points)
+	heartsLabel.text = "-" + str(hearts)
+	healthLabel.text = "P-" + str(health_points)
 	
 func _on_Timer_timeout() -> void:
 	print("tempo")
