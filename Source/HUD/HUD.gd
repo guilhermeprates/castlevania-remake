0e17b0336afbf6a638c8a7fe8ad4c51ee8669fd5
igcarol <111294070+igcarol@users.noreply.github.com>
2022-10-27 23:38:24 -0300

    Add files via upload

--- Source/HUD/HUD.gd
+++ Source/HUD/HUD.gd
@@ -1,10 +1,18 @@
 extends CanvasLayer
 
 onready var heartsLabel = $Control/HBoxContainer/HeartsLabel
-onready var healthLabel = $Control/HBoxContainer/HealthLabel
+onready var healthLabel = $Control/HBoxContainer2/HealthLabel
+onready var timerLabel = $Control/HBoxContainer4/TimerLabel
+var timer = 300
 
 func _process(delta: float) -> void:
 	var hearts = PlayerVariables.hearts
 	var health_points = PlayerVariables.health_points
 	heartsLabel.text = "- " + str(hearts)
-	healthLabel.text = "Health - " + str(health_points)
+	healthLabel.text = "P - " + str(health_points)
+	
+func _on_Timer_timeout() -> void:
+	print("tempo")
+	timer -= 1
+	timerLabel.text = "TIME 0" + str(timer)
+	
