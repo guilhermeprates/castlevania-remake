e1a49804deb132ed01707067931145996dab284a
Guilherme Prates <contact@guilhermeprates.com>
2022-10-28 21:41:41 -0300

    Update enemy

--- Source/Enemies/Zombie.gd
+++ Source/Enemies/Zombie.gd
@@ -9,13 +9,20 @@ export(int) var health_points: int = 1
 var _velocity: Vector2 = Vector2.ZERO
 var _move_direction = -1
 
-onready var hitbox: Area2D = $Hitbox
+onready var hitbox: Area2D = $Position2D/Hitbox
+onready var position2D: Position2D = $Position2D
 
 func _ready() -> void:
 	var _result = hitbox.connect("area_entered", self, "_on_area_entered")
 
 func _physics_process(delta: float) -> void:
 	if not _moving:
+		if _player_node.position.x > 0:
+			position2D.scale.x = -1 
+			_move_direction = 1
+		else:
+			position2D.scale.x = -1 
+			_move_direction = -1
 		_moving = distance_to_player() < target_distance
 	else:
 		_velocity.x = SPEED * _move_direction
@@ -25,9 +32,12 @@ func _physics_process(delta: float) -> void:
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Whip") and not _dead:
 		_dead = true
+		get_node("Position2D/Hitbox/CollisionHitbox").set_deferred("disable", true)
+		
 #		yield(get_tree().create_timer(0.2), "timeout")
 		$AnimationZombie.play("death")
+		
 		emit_signal("on_kill_enemy", position)
 		yield(get_tree().create_timer(0.4), "timeout")
 		queue_free() # inimigo vai morrer sem ter problemas na 
-		get_node("Hitbox/CollisionHitbox").set_deferred("disable", true)
+#		get_node("Position2D/Hitbox/CollisionHitbox").set_deferred("disable", true)
