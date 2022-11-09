8b48eafe72471a563b08e8a48d52ad0ec91d7cae
igcarol <111294070+igcarol@users.noreply.github.com>
2022-11-08 21:52:00 -0300

    Add files via upload

--- Source/Levels/Test/TestLevel.gd
+++ Source/Levels/Test/TestLevel.gd
@@ -1,13 +1,5 @@
 class_name TestLevel 
 extends Level
 
-onready var giantBat: GiantBat = $GiantBat
-onready var bossEventArea: Area2D = $BossEventArea
-
 func _ready() -> void:
-	var _result = bossEventArea.connect("area_entered", self, "_on_boss_event_area_entered")
-
-func _on_boss_event_area_entered(area: Area2D) -> void:
-	print("Boss Event")
-	if area.is_in_group("Player"):
-		giantBat.boss_event_trigged = true
+	pass
