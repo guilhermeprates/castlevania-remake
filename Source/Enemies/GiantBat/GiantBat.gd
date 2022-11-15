ca9d96fab9973f11c7c0d950a935101089e20a2f
Guilherme Prates <contact@guilhermeprates.com>
2022-11-15 17:37:38 -0300

    Update GiantBat

--- Source/Enemies/GiantBat/GiantBat.gd
+++ Source/Enemies/GiantBat/GiantBat.gd
@@ -1,11 +1,15 @@
 class_name GiantBat
 extends Boss
 
+enum State { IDLE, FLYING, ATTACKING, DEAD }
+
 var _time: int = 0
+var _state = State.IDLE
+
 var _trigged: bool = false
 var _attack_trigged: bool = false
 
-onready var timer: Timer = $Timer
+onready var attackTimer: Timer = $AttackTimer
 onready var position2D: Position2D = $Position2D
 onready var hitbox: Area2D = $Position2D/Hitbox
 onready var hitboxCollisionShape2D: CollisionShape2D = $Position2D/Hitbox/CollisionShape2D
@@ -13,40 +17,58 @@ onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	_experience = 3000
-	var _result = hitbox.connect("area_entered", self, "_on_area_entered")
-	animationPlayer.play("Idle") 
-	timer.set_wait_time(3.0)
-	timer.set_one_shot(false)
-	timer.connect("timeout", self, "_trigger_attack")
-	timer.start() 
+	_setup_connections()
+	_setup_attack_timer()
 
 func _physics_process(delta: float) -> void:
-	_time = delta
-	if boss_event_trigged and !_trigged:
-		animationPlayer.play("Flying") 
-	if not _dead and boss_event_trigged:
-		_trigged = true
-		_look_for_player()
-		if _attack_trigged:
+	_time = delta 
+	match(_state):
+		State.IDLE:
+			_idle(delta)
+		State.FLYING:
+			_fliying(delta)
+		State.ATTACKING:
 			_attack(delta)
-		else:
-			_retreat(delta)
+		State.DEAD:
+			_die(delta)
 
-func _trigger_attack() -> void:
-	_attack_trigged = true
+func _setup_attack_timer() -> void:
+	attackTimer.set_wait_time(3.0)
+	attackTimer.set_one_shot(false)
+	attackTimer.connect("timeout", self, "_update_state", [State.ATTACKING])
+	attackTimer.start()
+
+func _setup_connections() -> void:
+	var _result = hitbox.connect("area_entered", self, "_on_area_entered")
+
+func _update_state(state) -> void:
+	_state = state
+
+func _idle(delta: float) -> void:
+	print("STATE IDLE")
+	animationPlayer.play("Idle")
 
 func _attack(delta: float) -> void:
-	pass
+	if boss_event_trigged:
+		print("STATE ATTACK")
+		_look_for_player()
+		animationPlayer.play("Flying") 
 
-func _retreat(delta: float) -> void:
-	var target_position = _player_node.position
-	var direction_to_target = target_position - position
-	var distance_to_target = direction_to_target.length()
-	if distance_to_target < 200:
-		print(distance_to_target)
-		_velocity.x -= 10
-		_velocity = move_and_slide(_velocity, Vector2.UP)
-		
+func _fliying(delta: float) -> void:
+	if boss_event_trigged:
+		print("STATE FLYING")
+		animationPlayer.play("Flying")
+	
+func _die(delta: float) -> void:
+	if boss_event_trigged:
+		print("STATE DEAD")
+		hitboxCollisionShape2D.set_deferred("disable", true)
+		yield(get_tree().create_timer(0.2), "timeout")
+		animationPlayer.play("Death")
+		emit_signal("on_kill_boss", position)
+		yield(get_tree().create_timer(0.4), "timeout")
+		Game.player_score += _experience
+		queue_free()
 
 func _look_for_player() -> void:
 	if _player_node.position.x < position.x:
@@ -62,11 +84,4 @@ func _on_area_entered(area: Area2D) -> void:
 		health_points =- 1
 		Game.boss_health_points = health_points
 		if health_points == 0:
-			Game.player_score += _experience
-			_dead = true
-			hitboxCollisionShape2D.set_deferred("disable", true)
-			yield(get_tree().create_timer(0.2), "timeout")
-			animationPlayer.play("Death")
-			emit_signal("on_kill_boss", position)
-			yield(get_tree().create_timer(0.4), "timeout")
-			queue_free()
+			_update_state(State.DEAD)
--- Source/Levels/Test/TestLevel.gd
+++ Source/Levels/Test/TestLevel.gd
@@ -1,5 +1,14 @@
 class_name TestLevel 
 extends Level
 
+onready var giantBat: GiantBat = $GiantBat
+onready var bossEventArea: Area2D = $BossEventArea
+
 func _ready() -> void:
+	bossEventArea.connect("body_entered", self, "_on_boss_event_body_entered")
 	pass
+
+func _on_boss_event_body_entered(body: Node2D) -> void:
+	if body is Player:
+		print("BOSS EVENT AREA")
+		giantBat.boss_event_trigged = true
