17067f545ad7598ebdac816b4ca105a1d0b790f9
igcarol <111294070+igcarol@users.noreply.github.com>
2022-11-17 21:03:11 -0300

    Add files via upload

--- Source/Levels/Intro/Intro.gd
+++ Source/Levels/Intro/Intro.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _ready() -> void:
 	pass
+
+func _on_Area2D_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Player"):
+		get_tree().change_scene("res://Source/Levels/Level1/Level1.tscn")
