21bf368247406ae7d79269234997446281ed65bf
Guilherme Prates <contact@guilhermeprates.com>
2022-11-21 15:21:25 -0300

    Update Intro

--- Source/Levels/Intro/Intro.gd
+++ Source/Levels/Intro/Intro.gd
@@ -1,8 +1,16 @@
+class_name Intro
 extends Node2D
 
+onready var stage1OST: AudioStreamPlayer = $Stage1OST
+onready var castleSFX: AudioStreamPlayer = $CastleSFX
+
 func _ready() -> void:
-	pass
+	stage1OST.play()
+	castleSFX.connect("finished", self, "_on_castlesfx_finished")
 
 func _on_Area2D_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Player"):
-		get_tree().change_scene("res://Source/Levels/Level1/Level1.tscn")
+		castleSFX.play()
+		
+func _on_castlesfx_finished() -> void:
+	get_tree().change_scene("res://Source/Levels/Level1/Level1.tscn")
