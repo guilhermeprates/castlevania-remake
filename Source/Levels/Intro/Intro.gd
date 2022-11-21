585e0790f16dc1be71ce1f17bdab5e2a40b073b1
Guilherme Prates <contact@guilhermeprates.com>
2022-11-21 15:45:46 -0300

    Update Intro

--- Source/Levels/Intro/Intro.gd
+++ Source/Levels/Intro/Intro.gd
@@ -3,14 +3,16 @@ extends Node2D
 
 onready var stage1OST: AudioStreamPlayer = $Stage1OST
 onready var castleSFX: AudioStreamPlayer = $CastleSFX
+onready var timer: Timer = $Timer
 
 func _ready() -> void:
 	stage1OST.play()
-	castleSFX.connect("finished", self, "_on_castlesfx_finished")
+	timer.connect("timeout", self, "_on_timeout")
 
 func _on_Area2D_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Player"):
 		castleSFX.play()
-		
-func _on_castlesfx_finished() -> void:
+		timer.start()
+
+func _on_timeout() ->void:
 	get_tree().change_scene("res://Source/Levels/Level1/Level1.tscn")
