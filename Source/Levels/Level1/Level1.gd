427fb5a3bafc4bd3301857f86c9139b370d26d0d
Guilherme Prates <contact@guilhermeprates.com>
2022-11-21 11:57:24 -0300

    Add some OSTs

--- Source/Levels/Level1/Level1.gd
+++ Source/Levels/Level1/Level1.gd
@@ -1,12 +1,15 @@
 class_name Level1
 extends Level
 
+onready var stage1OST: AudioStreamPlayer = $Stage1OST
 onready var giantBat: GiantBat = $GiantBat
 onready var bossEventArea: Area2D = $BossEventArea
 
 func _ready() -> void:
+	stage1OST.play()
 	bossEventArea.connect("body_entered", self, "_on_boss_event_body_entered")
 
 func _on_boss_event_body_entered(body: Node2D) -> void:
 	if body is Player:
+		stage1OST.stop()
 		giantBat.boss_event_trigged = true
